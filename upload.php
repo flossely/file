ba0e5c6334a6a78df4534cc2b3bb8c559ef75bf6
Longinus <jesuscaesar@outlook.com>
2022-03-10 07:49:39 +0000

    Added upload function

---
+++ upload.php
@@ -0,0 +1,13 @@
+<?php
+$dir = $_POST['dir'];
+if (isset($_POST['submit'])) {
+    $count = count($_FILES['file']['name']);
+    for ($i = 0; $i < $count; $i++) {
+        if ($count <= 5) {
+            $tmp = $_FILES['file']['tmp_name'][$i];
+            $file = $_FILES['file']['name'][$i];
+            move_uploaded_file($tmp, $dir.'/'.$file);
+            chmod($dir.'/'.$file, 0777);
+        }
+    }
+}
